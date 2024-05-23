94a4c182fb25b079368948be4b533a8befed1ddc
Harshit Singla <harshitsingla1761@gmail.com>
2024-05-23 06:18:46 +0530

    Checking Something

--- index.js
+++ index.js
@@ -1,42 +1,55 @@
-const http = require("http");
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { PythonShell } = require('python-shell');
 
 const app = express();
 
-const path = require("path");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+const modelScript = 'model.py';
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "home.html"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"public", 'form.html'));
 });
 
-// function myHandler(req, res) {console.log("Request received");
-// const log = `Request received for ${req.url} at ${new Date()}\n`;
-// const myUrl = url.parse(req.url, true);
-// fs.appendFile("log.txt", log, (err, data) => {
-//     switch(myUrl.pathname) {
-//         case "/":
-//             res.end("Home Page");
-//             break;
-//         case "/about":
-//             const username = myUrl.query.username;
-//             res.end(`About Page for ${username}`);
-//             break;
-//         case "/contact":
-//             res.end("Contact Page");
-//             break;
-//         default:
-//             res.end("404 Page Not Found");
-//             break;
-//     }
-// })}
+// Define route for form submission
+app.post('/', (req, res) => {
+    console.log('Form submitted');
+    const { answers, tipi_answers, education, urban, gender, age_group, religion, orientation, race, married } = req.body;
 
-const myServer = http.createServer(app);
+    // Prepare data for model input
+    const inputData = {
+        answers: answers.split(','),
+        tipi_answers: tipi_answers.split(','),
+        education,
+        urban,
+        gender,
+        age_group,
+        religion,
+        orientation,
+        race,
+        married
+    };
 
-myServer.listen(8000, () => {
-    console.log("Server is running...");
+    // Call Python script for model prediction
+    PythonShell.run(modelScript, { args: [JSON.stringify(inputData)] }, (err, result) => {
+        if (err) {
+            console.error('Python script execution error:', err);
+            res.status(500).send('Internal server error');
+        } else {
+            console.log('Prediction result:', result);
+            res.render('result.html', { result: result[0] });
+        }
+    });
 });
 
+// Serve static files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
