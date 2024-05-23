0e73dc8898e37b5dcf9ca883e9796f5ab7899d5f
Harshit Singla <harshitsingla1761@gmail.com>
2024-05-23 05:51:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+// function myHandler(req, res) {console.log("Request received");
+// const log = `Request received for ${req.url} at ${new Date()}\n`;
+// const myUrl = url.parse(req.url, true);
+// fs.appendFile("log.txt", log, (err, data) => {
+//     switch(myUrl.pathname) {
+//         case "/":
+//             res.end("Home Page");
+//             break;
+//         case "/about":
+//             const username = myUrl.query.username;
+//             res.end(`About Page for ${username}`);
+//             break;
+//         case "/contact":
+//             res.end("Contact Page");
+//             break;
+//         default:
+//             res.end("404 Page Not Found");
+//             break;
+//     }
+// })}
+
+const myServer = http.createServer(app);
+
+myServer.listen(8000, () => {
+    console.log("Server is running...");
+});
+
+
+
