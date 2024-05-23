f1629b7b2a6d4265a8ef63190792ee64d9a80f14
Harshit Singla <harshitsingla1761@gmail.com>
2024-05-23 10:12:43 +0530

    Initiate

--- index.js
+++ index.js
@@ -1,50 +1,114 @@
 const express = require('express');
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // const { PythonShell } = require('python-shell');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+const questions = [
+    "I found myself getting upset by quite trivial things.",
+    "I was aware of dryness of my mouth.",
+    "I couldn't seem to experience any positive feeling at all.",
+    "I experienced breathing difficulty (eg, excessively rapid breathing, breathlessness in the absence of physical exertion).",
+    "I just couldn't seem to get going.",
+    "I tended to over-react to situations.",
+    "I had a feeling of shakiness (eg, legs going to give way).",
+    "I found it difficult to relax.",
+    "I found myself in situations that made me so anxious I was most relieved when they ended.",
+    "I felt that I had nothing to look forward to.",
+    "I found myself getting upset rather easily.",
+    "I felt that I was using a lot of nervous energy.",
+    "I felt sad and depressed.",
+    "I found myself getting impatient when I was delayed in any way (eg, elevators, traffic lights, being kept waiting).",
+    "I had a feeling of faintness.",
+    "I felt that I had lost interest in just about everything.",
+    "I felt I wasn't worth much as a person.",
+    "I felt that I was rather touchy.",
+    "I perspired noticeably (eg, hands sweaty) in the absence of high temperatures or physical exertion.",
+    "I felt scared without any good reason.",
+    "I felt that life wasn't worthwhile.",
+    "I found it hard to wind down.",
+    "I had difficulty in swallowing.",
+    "I couldn't seem to get any enjoyment out of the things I did.",
+    "I was aware of the action of my heart in the absence of physical exertion (eg, sense of heart rate increase, heart missing a beat).",
+    "I felt down-hearted and blue.",
+    "I found that I was very irritable.",
+    "I felt I was close to panic.",
+    "I found it hard to calm down after something upset me.",
+    "I feared that I would be 'thrown' by some trivial but unfamiliar task.",
+    "I was unable to become enthusiastic about anything.",
+    "I found it difficult to tolerate interruptions to what I was doing.",
+    "I was in a state of nervous tension.",
+    "I felt I was pretty worthless.",
+    "I was intolerant of anything that kept me from getting on with what I was doing.",
+    "I felt terrified.",
+    "I could see nothing in the future to be hopeful about.",
+    "I felt that life was meaningless.",
+    "I found myself getting agitated.",
+    "I was worried about situations in which I might panic and make a fool of myself.",
+    "I experienced trembling (eg, in the hands).",
+    "I found it difficult to work up the initiative to do things."
+]
+
+const tipi_questions = [
+    "Extraverted, enthusiastic.",
+    "Critical, quarrelsome.",
+    "Dependable, self-disciplined.",
+    "Anxious, easily upset.",
+    "Open to new experiences, complex.",
+    "Reserved, quiet.",
+    "Sympathetic, warm.",
+    "Disorganized, careless.",
+    "Calm, emotionally stable.",
+    "Conventional, uncreative."
+]
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // const modelScript = 'model.py';
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname,"public", 'form.html'));
-    res.render('home');
+    res.render('home', {questions : questions, tipi_questions : tipi_questions});
 });
 
-// // Define route for form submission
-// app.post('/', (req, res) => {
-//     console.log('Form submitted');
-//     const { answers, tipi_answers, education, urban, gender, age_group, religion, orientation, race, married } = req.body;
+// Define route for form submission
+app.post('/', (req, res) => {
+    console.log('Form submitted');
+    const { answers, tipi_answers, education, urban, gender, age_group, religion, orientation, race, married } = req.body;
 
-//     // Prepare data for model input
-//     const inputData = {
-//         answers: answers.split(','),
-//         tipi_answers: tipi_answers.split(','),
-//         education,
-//         urban,
-//         gender,
-//         age_group,
-//         religion,
-//         orientation,
-//         race,
-//         married
-//     };
+    const inputData = {
+        answers,
+        tipi_answers,
+        education,
+        urban,
+        gender,
+        age_group,
+        religion,
+        orientation,
+        race,
+        married
+    };
 
-//     // Call Python script for model prediction
-//     PythonShell.run(modelScript, { args: [JSON.stringify(inputData)] }, (err, result) => {
-//         if (err) {
-//             console.error('Python script execution error:', err);
-//             res.status(500).send('Internal server error');
-//         } else {
-//             console.log('Prediction result:', result);
-//         }
-//     });
-// });
+    console.log(inputData);
+    // console.log(typeof inputData)
+
+    res.render("result", {result : "Normal"});
+
+    // Call Python script for model prediction
+    // PythonShell.run(modelScript, { args: [JSON.stringify(inputData)] }, (err, result) => {
+    //     if (err) {
+    //         console.error('Python script execution error:', err);
+    //         res.status(500).send('Internal server error');
+    //     } else {
+    //         console.log('Prediction result:', result);
+    //     }
+    // });
+});
 
 // Serve static files (HTML, CSS, JS)
 
---
+++ public/js/form_new.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentQuestion = 0;
+    const totalQuestions = 60; // Update this to the actual number of questions
+    const questions = document.querySelectorAll('.question');
+    const prevButton = document.getElementById('prev-button');
+    const nextButton = document.getElementById('next-button');
+    const submitButton = document.getElementById('submit-button');
+    const fillBtn = document.getElementById('fillIt');
+
+    document.addEventListener("keydown", (event) => {
+        let buttons = questions[currentQuestion].querySelectorAll('input[type="radio"]');
+        let key = Number(event.key)-1;
+        if (key < buttons.length) {
+            fillBtn.textContent = "";
+            buttons[key].checked = true;
+        } else{
+           fillBtn.textContent = "Please Select within the Range";
+        }
+    })
+
+    function showQuestion(index) {
+        questions.forEach((question, i) => {
+            question.classList.toggle('active', i === index);
+        });
+    }
+
+    function updateButtons() {
+        prevButton.style.display = currentQuestion > 0 ? 'block' : 'none';
+        nextButton.style.display = currentQuestion < totalQuestions - 1 ? 'block' : 'none';
+        submitButton.style.display = currentQuestion === totalQuestions - 1 ? 'block' : 'none';
+    }
+
+    function validateCurrentQuestion() {
+        const currentQuestionElement = questions[currentQuestion];
+        const options = currentQuestionElement.querySelectorAll('input[type="radio"]');
+        return Array.from(options).some(option => option.checked);
+    }
+
+    const handlePrev = () => {
+        if (currentQuestion > 0) {
+            currentQuestion--;
+            showQuestion(currentQuestion);
+            updateButtons();
+        }
+    }
+
+    const handleNext = () => {
+        if (validateCurrentQuestion()) {
+            fillBtn.textContent = "";
+            if (currentQuestion < totalQuestions - 1) {
+                currentQuestion++;
+                showQuestion(currentQuestion);
+                updateButtons();
+            }
+        } else {
+            fillBtn.textContent = "Please select an option before proceeding !!";
+        }
+        
+    }
+
+    nextButton.addEventListener('click', handleNext);
+
+    prevButton.addEventListener('click', handlePrev);
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter' || event.keyCode === 13) {
+            event.preventDefault();
+            handleNext();
+        } else if (event.key === 'Backspace' || event.keyCode === 8) {
+            event.preventDefault();
+            handlePrev();
+        }
+    });
+
+    showQuestion(currentQuestion);
+    updateButtons();
+});
+
+console.log(document.getElementById("google_translate_element"));
