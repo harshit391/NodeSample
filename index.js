c70bb1ad0d5c85cf1b9e8ab80e753324f9f502ce
Harshit Singla <harshitsingla1761@gmail.com>
2024-05-23 06:55:19 +0530

    Check

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const { PythonShell } = require('python-shell');
+// const bodyParser = require('body-parser');
+// const { PythonShell } = require('python-shell');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-const modelScript = 'model.py';
+// const modelScript = 'model.py';
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"public", 'form.html'));
+    // res.sendFile(path.join(__dirname,"public", 'form.html'));
+    res.render('home');
 });
 
-// Define route for form submission
-app.post('/', (req, res) => {
-    console.log('Form submitted');
-    const { answers, tipi_answers, education, urban, gender, age_group, religion, orientation, race, married } = req.body;
+// // Define route for form submission
+// app.post('/', (req, res) => {
+//     console.log('Form submitted');
+//     const { answers, tipi_answers, education, urban, gender, age_group, religion, orientation, race, married } = req.body;
 
-    // Prepare data for model input
-    const inputData = {
-        answers: answers.split(','),
-        tipi_answers: tipi_answers.split(','),
-        education,
-        urban,
-        gender,
-        age_group,
-        religion,
-        orientation,
-        race,
-        married
-    };
+//     // Prepare data for model input
+//     const inputData = {
+//         answers: answers.split(','),
+//         tipi_answers: tipi_answers.split(','),
+//         education,
+//         urban,
+//         gender,
+//         age_group,
+//         religion,
+//         orientation,
+//         race,
+//         married
+//     };
 
-    // Call Python script for model prediction
-    PythonShell.run(modelScript, { args: [JSON.stringify(inputData)] }, (err, result) => {
-        if (err) {
-            console.error('Python script execution error:', err);
-            res.status(500).send('Internal server error');
-        } else {
-            console.log('Prediction result:', result);
-        }
-    });
-});
+//     // Call Python script for model prediction
+//     PythonShell.run(modelScript, { args: [JSON.stringify(inputData)] }, (err, result) => {
+//         if (err) {
+//             console.error('Python script execution error:', err);
+//             res.status(500).send('Internal server error');
+//         } else {
+//             console.log('Prediction result:', result);
+//         }
+//     });
+// });
 
 // Serve static files (HTML, CSS, JS)
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
